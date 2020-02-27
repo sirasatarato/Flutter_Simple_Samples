f509f9b057047ffeb2498ff0aa97e3b64840ac09
sirasatarato <sirasatarato@gmail.com>
2020-02-27 20:56:46 +0900

    et

---
+++ et/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var s = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(hintText: '써봐'),
+                onChanged: (String str){
+                  setState(() {
+                    s = str;
+                  });
+                },
+              ),
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              width: 300,
+            ),
+            Container(
+              child: Text(
+                s,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 44),
+              ),
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              width: 300,
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
